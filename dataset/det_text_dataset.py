f4bc1f955f29df6ebab5052a07849b2bbf845bcc
VeryGreatGuy <60505198+zhaozh10@users.noreply.github.com>
2023-06-22 22:04:59 +0800

    update

--- dataset/det_text_dataset.py
+++ dataset/det_text_dataset.py
@@ -3,6 +3,7 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 import json
+import os
 
 class DetTextSet(Dataset):
     def __init__(self, dataset_name: str, tokenized_dataset, transforms, log):
@@ -29,22 +30,24 @@ class DetTextSet(Dataset):
             bbox_labels = self.tokenized_dataset[index]["bbox_labels"]  # List[int]
             input_ids = self.tokenized_dataset[index]["input_ids"]  # List[List[int]]
             attention_mask = self.tokenized_dataset[index]["attention_mask"]  # List[List[int]]
+            bbox_phrases = self.tokenized_dataset[index]["bbox_phrases"]  # List[str]
             bbox_phrase_exists = self.tokenized_dataset[index]["bbox_phrase_exists"]  # List[bool]
             bbox_is_abnormal = self.tokenized_dataset[index]["bbox_is_abnormal"]  # List[bool]
 
             if self.dataset_name != "train":
-                # we only need the reference phrases during evaluation when computing scores for metrics
-                bbox_phrases = self.tokenized_dataset[index]["bbox_phrases"]  # List[str]
-
-                # same for the reference_report
+                # we only need the reference report during evaluation when computing scores for metrics
                 reference_report = self.tokenized_dataset[index]["reference_report"]  # str
 
-            # cv2.imread by default loads an image with 3 channels
-            # since we have grayscale images, we only have 1 channel and thus use cv2.IMREAD_UNCHANGED to read in the 1 channel
-            # image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-            # 随机生成数值在0-255之间的numpy.uint8数组
-            width, height=self.cxr_wh[image_path.replace("../data/mimic-cxr-reports/","")]['width'],self.cxr_wh[image_path.replace("../data/mimic-cxr-reports/","")]['height']
-            image = np.random.randint(0, 256, size=(height, width), dtype=np.uint8)
+            if image_path is None or not os.path.isfile(image_path):
+                # only used for local machine debugging (The MIMIC-CXR dataset has a size exceeding 500 GB)
+                print("[WARNING] Pseudo CXR now")
+                width, height=self.cxr_wh[image_path]['width'],self.cxr_wh[image_path]['height']
+                image = np.random.randint(0, 256, size=(height, width), dtype=np.uint8)
+            else:
+                # cv2.imread by default loads an image with 3 channels
+                # since we have grayscale images, we only have 1 channel and thus use cv2.IMREAD_UNCHANGED to read in the 1 channel
+                image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+
 
             # apply transformations to image, bbox_coordinates and bbox_labels
             transformed = self.transforms(image=image, bboxes=bbox_coordinates, class_labels=bbox_labels)
@@ -59,12 +62,12 @@ class DetTextSet(Dataset):
                 "bbox_labels": torch.tensor(transformed_bbox_labels, dtype=torch.int64),
                 "input_ids": input_ids,
                 "attention_mask": attention_mask,
+                "bbox_phrases":bbox_phrases,
                 "bbox_phrase_exists": torch.tensor(bbox_phrase_exists, dtype=torch.bool),
                 "bbox_is_abnormal": torch.tensor(bbox_is_abnormal, dtype=torch.bool),
             }
 
             if self.dataset_name != "train":
-                sample["bbox_phrases"] = bbox_phrases
                 sample["reference_report"] = reference_report
 
         except Exception as e:
--- main.py
+++ main.py
@@ -10,17 +10,18 @@ log = logging.getLogger(__name__)
 config_file_path='./run_config.txt'
 
 
-# datasets_as_df=get_datasets_as_df('./run_config.txt')
+
 train_transforms = get_transforms("train")
 # val_transforms = get_transforms("val")
 
 # tokenizer=get_tokenizer()
-# raw_train_dataset, raw_val_dataset = get_datasets(config_file_path)
-# tokenized_train_dataset, tokenized_val_dataset = get_tokenized_datasets(tokenizer, raw_train_dataset, raw_val_dataset)
 
-tokenized_train_dataset = load_from_disk("tokenized_train_dataset")
-tokenized_val_dataset = load_from_disk("tokenized_val_dataset")
-train_dataset = DetTextSet("train", tokenized_train_dataset, train_transforms, log)
+# load tokenized dataset
+tokenized_train = load_from_disk("tokenized_train")
+tokenized_val = load_from_disk("tokenized_val")
+tokenized_test=load_from_disk('tokenized_test')
+tokenized_test_2=load_from_disk('tokenized_test_2')
+train_dataset = DetTextSet("train", tokenized_train, train_transforms, log)
 # train_dataset = ObjectSet(datasets_as_df["train"], train_transforms)
-train_dataset.__getitem__(index=10)
+ret=train_dataset.__getitem__(index=10)
 print("hi")
