7c93d3f6cf5ab0c476ee453dfa03f41de5696c9c
VeryGreatGuy <60505198+zhaozh10@users.noreply.github.com>
2023-06-23 21:03:14 +0800

    update

--- gpt.py
+++ gpt.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, GPT2LMHeadModel
+import torch
 
 checkpoint = "healx/gpt-2-pubmed-medium"
 gpt_with_lm_head = GPT2LMHeadModel.from_pretrained(checkpoint)
@@ -10,3 +11,12 @@ gpt_children = list(gpt.children())
 
 wte = gpt_children[0] 
 
+# 指定形状
+shape = (3, 4)
+
+# 生成随机矩阵
+rand_matrix = torch.randint(low=10, high=101, size=shape, dtype=torch.long)
+
+
+inputs_embeds = wte(rand_matrix)
+print(f"inputs_embedding shape: {inputs_embeds.shape}")
